75cbae035cf8ffe117d46c6ff031314cb9347f01
minhl337 <imnotminh@gmail.com>
2019-04-03 10:04:45 -0500

    0 fails

---
+++ lib/dog.rb
@@ -0,0 +1,23 @@
+class Dog
+
+# def initialize(x)
+#     @name=x
+# end
+
+def name=(x)
+    @name=x
+end
+
+def name
+    @name
+end
+
+def breed=(x)
+    @breed=x
+end
+
+def breed
+    @breed
+end
+
+end
---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Person
+
+def name=(x)
+    @name=x
+end
+
+def name
+    @name
+end
+
+def job=(x)
+    @job=x
+end
+
+def job
+    @job
+end
+
+end
